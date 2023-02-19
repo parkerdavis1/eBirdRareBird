fdba1a6baf66f21fcf87b684e8d1453aa74aaa5f
Parker <parkied@gmail.com>
2023-02-18 18:42:23 -0700

    added region search+select

--- src/components/RegionForm.svelte
+++ src/components/RegionForm.svelte
@@ -1,7 +1,8 @@
 <script>
     import { enhance, applyAction } from '$app/forms';
     import { browser } from '$app/environment';
-    
+    import { debounce } from '../debounce';
+    export let regionData;
 
     import { region, loading } from '../store';
 
@@ -11,10 +12,67 @@
         //     document.getElementById('results-container').scrollIntoView()
         // }, 1000);
     }
+
+    let regionResultsList;
+    $: if (regionData) {
+        // console.log('regionData: ', regionData)
+        regionResultsList = regionData;
+        // selectOptions = regionData;
+    };
+
+    let regionSelect;
+    let regionSearchForm;
+    let regionInputElement;
+    let selectElement;
+
+    function regionInputChange() {
+        console.log('HELLO')
+        regionSearchForm.requestSubmit();
+        regionInputElement.blur();
+    }
+ 
 </script>
 
+<form method="POST" bind:this={regionSearchForm} id="regionSearch" action="?/regionSearch" on:submit|preventDefault use:enhance>
+    <div>
+        <!-- <label for="regionInput">Region Search</label> -->
+        <input 
+            type="text" 
+            id="regionInput" 
+            name="regionSearch" 
+            form="regionSearch" 
+            bind:value={regionSelect} 
+            bind:this={regionInputElement}
+            on:input={debounce(regionInputChange, 2000)}
+            required 
+            autocomplete="off"
+            placeholder="Search for region"
+            >
+        <!-- <button type="submit">Region Search</button> -->
+        {#if regionResultsList}
+        <br>
+        <label for="regionSelect">Select a region:</label>
+        <select 
+            name="region" 
+            form="region" 
+            id="regionSelect" 
+            bind:value={$region.region} 
+            bind:this={selectElement}
+            >
 
-<form method="POST" action="?/region" class="container" use:enhance=
+            {#each regionResultsList as option}
+                {#if regionResultsList.length === 1}
+                <option value={option.code} selected>{option.name}</option>
+                {:else}
+                <option value={option.code}>{option.name}</option>
+                {/if}
+            {/each}
+        </select>
+        {/if}
+    </div>
+</form>
+
+<form method="POST" id="region" action="?/region" class="container" use:enhance=
     {({ form, data, action }) => {
     // before form is submitted
     // console.log('form: ', form)
@@ -29,7 +87,7 @@
     }
 }}>
     <div class="grid">
-        <div>
+        <!-- <div>
             <label for="region">Region Code:</label>
             <select name="region" id="region" bind:value={$region.region}>
                 <option value="US-AZ-001">Apache</option>
@@ -47,15 +105,14 @@
                 <option value="US-AZ-023">Santa Cruz</option>
                 <option value="US-AZ-025">Yavapai</option>
                 <option value="US-AZ-027">Yuma</option>
-                <!-- <option value="US-AZ">Arizona</option> -->
             </select>
-        </div>
-
+        </div> -->
+            
         <label for="days">Time period (days ago): {$region.days}</label>
         <input type="range" name="days" id="days" min="1" max="30" bind:value={$region.days}>
 
     </div>
-    <button type="Submit" class="submit-button" on:click={handleSubmit}>
+    <button type="submit" class="submit-button" on:click={handleSubmit}>
         {$loading ? "Loading..." : "Submit"}
     </button>
 
