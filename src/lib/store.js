63b9c6fa881d5f77f12a63f95f9f2755767fd992
Parker <parkied@gmail.com>
2023-02-24 11:33:11 -0700

    update stores to be session stored

--- src/lib/store.js
+++ src/lib/store.js
@@ -5,10 +5,10 @@ export const loading = writable(false);
 
 export const allComments = writable({});
 
-export const isRadiusView = writable(browser ? JSON.parse(localStorage.getItem('isRadiusView')) || false : false);
+export const isRadiusView = writable(browser ? JSON.parse(sessionStorage.getItem('isRadiusView')) || false : false);
 isRadiusView.subscribe(value => {
     if (browser) {
-        localStorage.isRadiusView = JSON.stringify(value);
+        sessionStorage.isRadiusView = JSON.stringify(value);
     }
 })
 
@@ -19,19 +19,19 @@ latLon.subscribe( value => {
     }
 })
 
-let radiusDefault = {latLon: undefined, distance: 20, days: 7}
-export const radius = writable(browser ? JSON.parse(localStorage.getItem('radiusFormInputs')) || radiusDefault : radiusDefault);
+let radiusDefault = {latLon: undefined, distance: 20, days: 14}
+export const radius = writable(browser ? JSON.parse(sessionStorage.getItem('radiusFormInputs')) || radiusDefault : radiusDefault);
 radius.subscribe(value => {
     if (browser) {
-        localStorage.radiusFormInputs = JSON.stringify(value)
+        sessionStorage.radiusFormInputs = JSON.stringify(value)
     }
 });
 
 let regionDefault = { days: 14, region: 'US-AZ-013'}
-export const region = writable(browser ? JSON.parse(localStorage.getItem('regionFormInputs')) || regionDefault : regionDefault)
+export const region = writable(browser ? JSON.parse(sessionStorage.getItem('regionFormInputs')) || regionDefault : regionDefault)
 region.subscribe(value => {
     if (browser) {
-        localStorage.regionFormInputs = JSON.stringify(value)
+        sessionStorage.regionFormInputs = JSON.stringify(value)
     }
 });
 
