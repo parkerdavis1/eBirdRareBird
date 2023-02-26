91b0c585b4b3a2f70a1e5170b583e458f4cde4ff
Parker <parkied@gmail.com>
2023-02-26 10:13:31 -0700

    region -> county

--- src/lib/components/LocationModal.svelte
+++ src/lib/components/LocationModal.svelte
@@ -16,7 +16,7 @@
 
             border border-1 border-black" transition:fade>
     <div class="max-w-4xl mx-auto p-4">
-        <h2 class="font-bold mb-4">Region</h2>
+        <h2 class="font-bold mb-4">County</h2>
         <div class="max-w-md">
             <AltRegionForm 
                 regionData={regionData}
