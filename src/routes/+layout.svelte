4a7dfd64a4c5aa4a8939db4670b0c8b49e18418c
Parker <parkied@gmail.com>
2023-11-27 12:11:30 -0700

    + Plausible analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import '$lib/css/app.css';
 </script>
+<svelte:head>
+    <script defer data-domain="rarebird.parkerdavis.dev" src="https://plausible.parkerdavis.dev/js/script.js"></script>
+</svelte:head>
 
 <slot></slot>
